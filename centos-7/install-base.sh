fe3444b5f10f39bee810c87d6cfee98f20ae8764
dpmatthews <david.matthews@metoffice.gov.uk>
2015-08-25 11:14:00 +0100

    Update software versions FCM_VERSION=2015.08.0 CYLC_VERSION=6.6.0 ROSE_VERSION=2015.08.0

--- centos-7/install-base.sh
+++ centos-7/install-base.sh
@@ -1,7 +1,7 @@
 # Define software versions
-FCM_VERSION=2015.07.0
-CYLC_VERSION=6.5.0
-ROSE_VERSION=2015.06.0
+FCM_VERSION=2015.08.0
+CYLC_VERSION=6.6.0
+ROSE_VERSION=2015.08.0
 
 #### Install commonly used editors
 yum install -y gedit gvim emacs
