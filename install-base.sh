00a98b82c12e748913101df9a1761ebc6075617c
dpmatthews <david.matthews@metoffice.gov.uk>
2016-04-29 14:39:38 +0100

    Add UM setup files

--- install-base.sh
+++ install-base.sh
@@ -219,3 +219,5 @@ fi
 dos2unix -n /vagrant/usr/local/bin/run-test-batteries /usr/local/bin/run-test-batteries
 dos2unix -n /vagrant/usr/local/bin/install-jules-extras /usr/local/bin/install-jules-extras
 dos2unix -n /vagrant/usr/local/bin/install-jules-gswp2-data /usr/local/bin/install-jules-gswp2-data
+dos2unix -n /vagrant/usr/local/bin/install-um-extras /usr/local/bin/install-um-extras
+dos2unix -n /vagrant/usr/local/bin/um-setup /usr/local/bin/um-setup
